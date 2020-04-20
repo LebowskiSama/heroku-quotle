0c5d30dec37a039a8b535f608d60bb39980b4a4c
LebowskiSama <anushvenkat101@gmail.com>
2020-04-20 11:45:29 +0530

    added-image-error-handling

--- monkey/static/js/vueSingle.js
+++ monkey/static/js/vueSingle.js
@@ -12,7 +12,7 @@ var searchField = new Vue({
     <v-layout row wrap justify="space-between">
       <v-flex xs12 sm6 md4 lg3 v-for="item in suggestions" :key="item.imdbID">
         <v-card raised width="200" raised class="text-xs-center ma-3" @click="parseChoice(item.imdbID)">
-        <img :src="item.Poster" height="300" width="200" contain></img>
+        <img :src="item.Poster" @error="empytyHandle" height="300" width="200" contain></img>
         <v-card-subtitle justify-content>{{ item.Title }}</v-card-subtitle>
         </v-card>
       </v-flex>
@@ -24,10 +24,14 @@ var searchField = new Vue({
 </div>`,
   methods:{
     callOMDB: function(){
+      this.quotes = null;
       axios({
         method: "get",
         url: "http://www.omdbapi.com/?apikey=d0b356ff&s=" + this.search,
       }).then((response) => (this.suggestions = response.Search));
+    },
+    empytyHandle: function(event){
+      event.target.src = "https://i.pinimg.com/originals/fe/b0/32/feb032472adaa49c69e7848eefda4d7f.jpg"
     },
     parseChoice: function(itemid){
       axios({
@@ -37,11 +41,7 @@ var searchField = new Vue({
           ID: itemid
         }})
         .then(jsonData => this.quotes=jsonData.quotes)
-        this.$nextTick(() => { 
-          var objDiv = document.getElementById("quotes");
-          objDiv.scrollIntoView({behavior: "smooth",})
-
-        })
+        document.getElementById("quotes").scrollIntoView({behavior: "smooth"})
     }
   }
 });
